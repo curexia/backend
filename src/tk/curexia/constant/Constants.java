2c1ca4651a267643f96f4b3e32174925dac1e791
Aditya Shrivastava <aditya.shrivastava.716@gmail.com>
2021-06-12 20:11:54 +0530

    Remove Health OS

--- src/tk/curexia/constant/Constants.java
+++ src/tk/curexia/constant/Constants.java
@@ -39,12 +39,5 @@ public class Constants {
         return "Curexia!";
     }
 
-    public static String healthOSID(){
-        return "b3765edad9e72491f0ec466d1244f906b3f11a147f550f58fc28ad500eddcb44";
-    }
-    public static String healthOSSecret(){
-        return "d30ed9dd05e9130d37e477c0ba234efe6a6e65dcf8deda57fa037a50e3c9f083";
-    }
-    public static String healthOSAccessToken(){ return "1b53411411cc1ab9de8325cd63a67fe23259747b09fa47eaca7b36c11ae5784d";}
     public static String getUserAgent(){return USER_AGENT;};
 }
